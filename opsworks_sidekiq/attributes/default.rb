26bef40d7ecca7438de82673bdca2dbfad0678e6
Moya <j.kang@kagunova.com>
2015-06-16 13:57:19 +0900

    Update default.rb

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -5,6 +5,7 @@ default[:sidekiq] = {}
 node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern] = {}
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
+  override['sidekiq']['kagunova']['worker']['config']['logfile'] = '/var/log/sidekiq_worker'
   default[:sidekiq][application.intern][:syslog] = false
 end
 
